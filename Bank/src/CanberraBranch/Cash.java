8c0250953254d99325714790d137da8c2013a98f
willowywispp <willow.woods142@gmail.com>
2026-09-02 00:45:22 +1000

    bank project

---
+++ Bank/src/CanberraBranch/Cash.java
@@ -0,0 +1,10 @@
+package CanberraBranch;
+
+public class Cash {
+
+    static boolean verifyCashAmount() {
+        return true;
+    }
+
+    }
+
---
+++ printname/src/name/name.java
@@ -0,0 +1,13 @@
+package name;
+
+import java.util.Scanner;
+
+public class name {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Your Name: ");
+        String name = scanner.nextLine();
+        System.out.println("Your name is: " + name);
+
+    }
+}
